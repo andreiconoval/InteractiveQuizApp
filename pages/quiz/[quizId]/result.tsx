98578408db8ec3bd43fde02a648930d892e54ab6
Andrei Conoval <andreiconoval@gmail.com>
2024-10-27 12:35:29 +0200

    [Fake] Changes for module 4

--- pages/quiz/[quizId]/result.tsx
+++ pages/quiz/[quizId]/result.tsx
@@ -3,39 +3,53 @@ import DefaultLayout from "@/layouts/default";
 import { Quiz } from "@/types";
 import { Chip } from "@nextui-org/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export interface ScorePageProps {
-  quiz: Quiz;
-}
-
-export default function ScoreDetail({ quiz }: ScorePageProps) {
+export default function ScoreDetail() {
   const router = useRouter();
   const { query } = router;
-  if (!quiz) return <p>Quiz not found</p>;
+  const [quiz, setQuiz] = useState<Quiz | undefined>(undefined);
+
+  useEffect(() => {
+    if (router.query.quizId) {
+      loadQuiz(+router.query.quizId);
+    }
+  }, [router.query.quizId]);
 
   return (
-    <DefaultLayout>
-      <section className="flex flex-col items-center align-middle justify-center gap-4 py-4 md:py-4 h-full">
-        <div className="inline-block max-w-xl text-center justify-center">
-          <span className={title()}>{quiz.title}</span>
-          <br />
-          <div className={subtitle({ class: "mt-4" })}>{quiz.description}</div>
-          <br />
-          <div className={subtitle({ class: "mt-4" })}>
-            Your score is: ({query.score} points!) from{" "}
-            {quiz?.questions?.length} points
+    quiz && (
+      <DefaultLayout>
+        <section className="flex flex-col items-center align-middle justify-center gap-4 py-4 md:py-4 h-full">
+          <div className="inline-block max-w-xl text-center justify-center">
+            <span className={title()}>{quiz?.title}</span>
+            <br />
+            <div className={subtitle({ class: "mt-4" })}>
+              {quiz?.description}
+            </div>
+            <br />
+            <div className={subtitle({ class: "mt-4" })}>
+              Your score is: ({query.score} points!) from{" "}
+              {quiz?.questions?.length} points
+            </div>
+            <div>
+              {+query?.score! >= quiz?.questions?.length! * 0.7 ? (
+                <Chip color="success">You did a great job </Chip>
+              ) : (
+                <Chip color="danger">Try again</Chip>
+              )}
+            </div>
           </div>
-          <div>
-            {+query?.score! >= quiz?.questions?.length * 0.7 ? (
-              <Chip color="success">You did a great job </Chip>
-            ) : (
-              <Chip color="danger">Try again</Chip>
-            )}
-          </div>
-        </div>
-      </section>
-    </DefaultLayout>
+        </section>
+      </DefaultLayout>
+    )
   );
+
+  async function loadQuiz(quizId: number) {
+    const request = await fetch(`http://localhost:3000/api/quiz/${quizId}`);
+    const quiz = await request.json();
+
+    setQuiz(quiz);
+  }
 }
 
 export async function getServerSideProps(context: any) {
